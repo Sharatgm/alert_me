9802db6a6119e138d0ea48ef38f172c5db605c28
Sharatgm <A01205254@itesm.mx>
2018-04-12 17:33:40 -0500

    Appearance details

--- app/src/main/java/mx/itesm/alert_me/MainActivity.java
+++ app/src/main/java/mx/itesm/alert_me/MainActivity.java
@@ -1,10 +1,15 @@
 package mx.itesm.alert_me;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
 
         Button Proximity = (Button) findViewById(R.id.button);
         Button Doors = (Button) findViewById(R.id.button2);
+        Proximity.setBackgroundColor(Color.parseColor("#ffbd4a"));
 
         Proximity.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,5 +37,32 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(one);
             }
         });
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
+
+        // Activar flechita hacia atrás
+
     }
+
+    // Agregar menú del resource file llamado menu
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    // Acciones al presionar cada botón
+    public boolean onOptionsItemSelected(MenuItem menuItem){
+        switch (menuItem.getItemId()){
+            case R.id.settings:
+                Toast.makeText(this,"Has pulsado el botón borrar", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.logout:
+                Toast.makeText(this, "Has pulsado el botón editar", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return true;
+    }
+
 }
+
+
--- app/src/main/java/mx/itesm/alert_me/ProxActivity.java
+++ app/src/main/java/mx/itesm/alert_me/ProxActivity.java
@@ -2,6 +2,10 @@ package mx.itesm.alert_me;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class ProxActivity extends AppCompatActivity {
 
@@ -9,5 +13,33 @@ public class ProxActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prox);
+
+        // Agregar toolbar del xml llamado toolbar
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
+
+        // Activar flechita hacia atrás
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+    // Agregar menú del resource file llamado menu
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    // Acciones al presionar cada botón
+    public boolean onOptionsItemSelected(MenuItem menuItem){
+        switch (menuItem.getItemId()){
+            case R.id.settings:
+                Toast.makeText(this,"Has pulsado el botón borrar", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.logout:
+                Toast.makeText(this, "Has pulsado el botón editar", Toast.LENGTH_SHORT).show();
+                break;
+            case android.R.id.home:
+                finish();
+                break;
+        }
+        return true;
     }
 }
