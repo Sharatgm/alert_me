90bab54158626b0e612f21479ef4815927042dd3
Inpirncurse <A01204805@itesm.mx>
2018-05-07 12:07:23 -0500

    complete token id

--- app/src/main/java/mx/itesm/alert_me/FirebaseInstanceIDService.java
+++ app/src/main/java/mx/itesm/alert_me/FirebaseInstanceIDService.java
@@ -1,4 +1,21 @@
 package mx.itesm.alert_me;
 
-public class FirebaseInstanceIDService {
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+import android.util.Log;
+
+import static android.content.ContentValues.TAG;
+
+public class FirebaseInstanceIDService extends FirebaseInstanceIdService {
+
+    public static final String REG_TOKEN = "REG_TOKEN";
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(REG_TOKEN, "Refreshed token: " + refreshedToken);
+    }
+
+
 }
